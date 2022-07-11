1104ba041fceb22179a7a1be4c93b2a521d6ec5d
Marco Cheung <73602420+LAFLAMIE1024@users.noreply.github.com>
2022-07-11 14:48:30 +0800

    Update util.py

--- util.py
+++ util.py
@@ -1,5 +1,12 @@
 import importlib
 
+def get_obj_from_str(string, reload=False):
+    module, cls = string.rsplit(".", 1)
+    if reload:
+        module_imp = importlib.import_module(module)
+        importlib.reload(module_imp)
+    return getattr(importlib.import_module(module, package=None), cls)
+
 def instantiate_from_config(config, params = None):
     if not "target" in config:
         raise KeyError("Expected key `target` to instantiate.")
@@ -7,10 +14,3 @@ def instantiate_from_config(config, params = None):
         return get_obj_from_str(config["target"])(**config.get("params", dict()))
     else:
         return get_obj_from_str(config["target"])(**params, **config.get("params", dict()))
-
-def get_obj_from_str(string, reload=False):
-    module, cls = string.rsplit(".", 1)
-    if reload:
-        module_imp = importlib.import_module(module)
-        importlib.reload(module_imp)
-    return getattr(importlib.import_module(module, package=None), cls)
